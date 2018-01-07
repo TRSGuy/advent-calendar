8b95aa2c93661de9cf9aeceaccf1d119b1e17092
Kim Karlsson <vorap@voxelnet.io>
2018-01-07 13:33:58 +0100

    Let's just call it bugfixes

--- js/game.js
+++ js/game.js
@@ -1,12 +1,27 @@
+/*
+* KeyCodes: [[LEFT, 37], [UP, 38], [RIGHT, 39], [DOWN, 40]]
+* Thanks to: http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/ for helping me figure out the timing and the inputs (since I'm so horrible at maths)
+*/
 var ctx;
 var game = {
-    player: {
-	x: 225,
-	y: 255,
-	size: 50,
-	speed: 4
-    },
     keys: {}
+}
+function generatePlayer() {
+    this.x = 225;
+    this.y = 255;
+    this.size = 50;
+    this.speed = 4;
+    this.updatePosition = function (keyCodes, modifier) {
+	if(37 in keyCodes) { // move:left
+	    this,x =- this.speed * modifier;
+	} if(38 in keyCodes) { // move:up
+	    this.y =- this.speed * modifier;
+	} if(39 in keyCodes) { // move:right
+	    this.x =+ this.speed * modifier;
+	} if(40 in keyCodes) { // move:down
+	    this.y =+ this.speed * modifier;
+	}
+    }
 }
 function loop() {
     ctx.clearRect(0, 0, 500, 500);
@@ -25,10 +40,8 @@ $(document).ready(function() {
     });
 });
 function update() {
-    
 }
 function render() {
-    
 }
 function loop() {
     update();
