8b78c97d6f18de58a141758fb35c4bf6866eb336
Kim <kimpankarl@gmail.com>
2018-01-07 17:06:25 +0100

    Added second animation
    Added the animation behind the second hatch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+function appendClass(_class, obj) {
+	obj.addClass(_class);
+	console.log("Added " + _class + "to object");
+}
 $(document).ready(function() {
 	$(".hatch-label").on("click", function() {
 	    $(this).toggleClass("open");
 	});
+	for (var i = 1; i <= 5; i++) {
+		$(".anim-2-bar-" + i).css("animation-delay", "0." + i + "s")
+	}
 });
