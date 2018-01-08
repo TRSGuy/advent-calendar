6e00f74c7986c034934d6c1c76b4c82d63d6aad6
Kim <kimpankarl@gmail.com>
2018-01-08 19:09:17 +0100

    Commented css and arrays start at zero
    Commented each block in the css and changed the numbering of the animations, arrays starts at zero. TODO: Generate the html using js.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
+var animations = [
+	{
+		"name": "anim-0",
+		"objects": [[4, "ball"]]
+	}
+];
 function appendClass(_class, obj) {
 	obj.addClass(_class);
 	console.log("Added " + _class + "to object");
 }
 $(document).ready(function() {
-	$(".hatch-label").on("click", function() {
+	$(".hatch-face").on("click", function() {
 	    $(this).toggleClass("open");
 	});
 	for (var i = 1; i <= 5; i++) {
 		if(i <= 4) {
-			$(".anim-3-square-" + i).css("animation-delay", "0." + i + "s");
-			$(".anim-4-bar-" + i).css("animation-delay", "0." + (i + 2) + "s");
+			$(".anim-2-square-" + i).css("animation-delay", "0." + i + "s");
+			$(".anim-3-bar-" + i).css("animation-delay", "0." + (i + 2) + "s");
 		}
-		$(".anim-2-bar-" + i).css("animation-delay", "0." + i + "s");
+		$(".anim-1-bar-" + i).css("animation-delay", "0." + i + "s");
 	}
 });
