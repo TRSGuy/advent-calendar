c9e51fcf8739b90a7eee96f77d0cde04995f7779
Kim <kimpankarl@gmail.com>
2018-01-09 17:24:17 +0100

    Made the html self generating
    Managed to make the html self generating

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 * Including a responsive canvas game built for both computers and tablets
 ## How
 * Using CSS3 and use Translating and 3DTransform to get the "hatches" moving as we want to
-* Using the grid system + min-width to get this whole thing responsive
+* Using the grid system to get this whole thing responsive
 * Canvas Game:
   * Keyboard controls on computer and touch controls on mobile/tablet
--- js/main.js
+++ js/main.js
@@ -1,22 +1,55 @@
-var animations = [
+var hatches = [
 	{
-		"name": "anim-0",
-		"objects": [[4, "ball"]]
+		"hatch-content": {
+			"animation": {
+				"objects": [[4, "ball"]]
+			}
+		}
+	}, {
+		"hatch-content": {
+			"animation": {
+				"delay": 0.1,
+				"objects": [[5, "bar"]]
+			}
+		}
+	}, {
+		"hatch-content": {
+			"animation": {
+				"delay": 0.1,
+				"objects": [[4, "square"]]
+			}
+		}
+	}, {
+		"hatch-content": {
+			"animation": {
+				"delay": 0.1,
+				"objects": [[4, "bar"]]
+			}
+		}
 	}
 ];
-function appendClass(_class, obj) {
-	obj.addClass(_class);
-	console.log("Added " + _class + "to object");
-}
+var hatchElements = [];
 $(document).ready(function() {
+	for (var hatchIter = 0; hatchIter < hatches.length; hatchIter++) { // For each hatch
+		var hatch = hatches[hatchIter];
+		var hatchElement = $("<div/>").addClass("hatch col-3");
+		hatchElement.append($("<div/>").addClass("hatch-face").text(hatchIter + 1));
+		hatchElement.append($("<div/>").addClass("hatch-content"));
+		if(hatch["hatch-content"]["animation"]) { // It is an animation
+			var animationContainer = $("<div/>").addClass("animation-container");
+			var animation = hatch["hatch-content"]["animation"];
+			for (var objectTypeIter = 0; objectTypeIter < animation["objects"].length; objectTypeIter++) { // For each type of object in animation
+				for (var objectCountIter = 0; objectCountIter < animation["objects"][objectTypeIter][0]; objectCountIter++) { // For each object of that type
+					var animationElementClass = "anim-obj " + "anim-" + hatchIter + "-" + animation["objects"][objectTypeIter][1] + "-" + objectCountIter;
+					var animationElement = $("<div/>").addClass(animationElementClass).css("animation-delay", animation["delay"] * objectCountIter+"s");
+					animationContainer.append(animationElement);
+					hatchElement.children().eq(1).append(animationContainer);
+				}
+			}
+		}
+		$("#hatches-container").append(hatchElement);
+	}
 	$(".hatch-face").on("click", function() {
 	    $(this).toggleClass("open");
 	});
-	for (var i = 1; i <= 5; i++) {
-		if(i <= 4) {
-			$(".anim-2-square-" + i).css("animation-delay", "0." + i + "s");
-			$(".anim-3-bar-" + i).css("animation-delay", "0." + (i + 2) + "s");
-		}
-		$(".anim-1-bar-" + i).css("animation-delay", "0." + i + "s");
-	}
 });
